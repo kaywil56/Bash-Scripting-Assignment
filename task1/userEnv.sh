9dc9853e3aa46657c28d8a22e914cd2db77465e7
Kaylem W <willkj8@student.op.ac.nz>
2021-09-08 21:19:30 +1200

    output test user username

--- task1/userEnv.sh
+++ task1/userEnv.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
+# Checks if input is a local file or URI
 processInput(){
-
-    if [[ ${filename:0:4} == http ]]; then
-            echo URI
-            wget $filename
-    else
-            echo Not URI
-            cp $filename .
-    fi
+	if [[ ${filename:0:4} == http ]]; then
+		echo URI
+        wget $filename
+	else
+        echo Not URI
+        cp $filename .
+	fi
 }
 
 filename=$1
 
+# Checks if the user the user has added a param to the executable
 if [ -z $filename ]; then
 		read -p "CSV file location: " filename
 		processInput $filename
@@ -24,7 +25,12 @@ IFS=";"
 
 while read col1 col2 col3 col4
 do
-	echo email $col1
+	firstname=${col1:0:1}
+	temp=${col1%@*}
+	lastname=${temp#*.}
+	username=$firstname$lastname
+	
+	echo $username
 	echo DOB $col2
 	echo groups $col3
 	echo sharedFolder $col4
