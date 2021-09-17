aaee2fa150ed9c7b4d669b0d5d119d5661a23986
root <root@IN616OSC10.2kbzwsj0utcevdbqinoimzrskc.qx.internal.cloudapp.net>
2021-09-17 22:59:06 +0000

    Reset repo

--- task1/userEnv.sh
+++ task1/userEnv.sh
@@ -4,45 +4,40 @@ clear
 
 # Checks if input is a local file or URI
 processInput(){
-        if [[ ${filename:0:4} == http ]]; then
-                echo URI
-        wget $filename
-                filename=${filename##*/}
-        else
-        echo Not URI
-        cp $filename .
-        fi
+	if [[ ${filename:0:4} == http ]]; then
+        	wget $filename
+		filename=${filename##*/}
+	else
+        	cp $filename .
+	fi
 }
 # Checks if secondary groups exist. If not they are created
 checkGroups(){
-        IFS=","
-        for value in $groups
-        do
-                grep $value /etc/group
-                if [[ $? == 1 ]]; then
-                        sudo groupadd $value
- sudo groupadd $value
-                        echo The group $value was created.
-                fi
-        done
+	IFS=","
+	for value in $groups
+	do
+		grep $value /etc/group
+		if [[ $? == 1 ]]; then
+			sudo groupadd $value
+		fi
+	done
 }
 # Checks if shared folders exist. If not they are created
 checkSharedFolders(){
-        cd /
-        find $shared
-                if [[ $? == 1 ]]; then
-                        sudo mkdir $shared
-                        echo shared folder $shared created
-                fi
-        cd
+	cd /
+	find $shared 2> /dev/null 
+		if [[ $? == 1 ]]; then
+		 	sudo mkdir $shared 
+		fi
+	cd
 }
 
 filename=$1
 
 # Checks if the user the user has added a param to the executable
 if [ -z $filename ]; then
-                read -p "CSV file location: " filename
-                processInput $filename
+		read -p "CSV file location: " filename
+		processInput $filename
 else
                 processInput $filename
 fi
@@ -53,51 +48,65 @@ count=0
 while read user
 do
         ((count++))
-        echo $count
 
 done < $filename
 
 IFS=';'
 
+count=$(($count-1))
+
 echo ========== User Enviroment ==========
+echo 
 echo Users to be created: $count
-echo
+echo 
 echo Do you wish to run this command? yes/no:
 read -p ">>> " yesorno
 
 if [[ $yesorno == no ]]; then
-        exit
+	exit
 fi
+
+count=0
 
 while read email DOB groups shared
 do
-        # Creates a username from the given email
-        firstChar=${email:0:1}
-        stripEmail=${email%@*}
-        lastname=${stripEmail#*.}
-        username=$firstChar$lastname
+	if [[ $count > 1 ]]; then
 
-        # Creates a password from a given DOB
-        year=$(echo $DOB | cut -d '/' -f 1)
-        month=$(echo $DOB | cut -d '/' -f 2)
-        password=$month$year
-        echo $password
-       # Checks if secondary groups exist
-        checkGroups $groups
-        IFS=";"
+		# Creates a username from the given email
+		firstChar=${email:0:1}
+		stripEmail=${email%@*}
+		lastname=${stripEmail#*.}
+		username=$firstChar$lastname
 
-        # Checks if shared exist
-        checkSharedFolders $shared
+		# Creates a password from a given DOB
+		year=$(echo $DOB | cut -d '/' -f 1)
+		month=$(echo $DOB | cut -d '/' -f 2)
+		password=$month$year
+	
+		# Checks if secondary groups exist
+		#checkGroups $groups
+		IFS=";"
+	
+		# Checks if shared exist
+		###################################################	checkSharedFolders $shared
+	
+		#echo creates user
+	#	sudo useradd -d /home/$username -m -G $groups $username -m
+	
+		#Adds password to the user
+		#sudo echo "$username:$password" | sudo chpasswd
 
-        #echo creates user
-        sudo useradd -d /home/$username -m -G $groups $username -m
-        echo User $username created.
+        	#Summary of created user
+		echo User $username created
+		echo -------------------------------------
+		echo Home directory: /home/$username
+		echo Shared folders: $shared
+        	echo Password: $"******"
+		echo Groups: $groups	
+		echo =====================================
+	fi
 
-        #Adds password to the user
-        sudo echo "$username:$password" | sudo chpasswd
-        echo =====================================
-
+	((count++))
+	
 done < $filename
 
-
-                                                          
