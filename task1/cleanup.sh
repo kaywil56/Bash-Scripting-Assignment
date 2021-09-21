6e7d20e51df63d81160c4a0ed716e0a9751029c4
root <root@IN616OSC10.2kbzwsj0utcevdbqinoimzrskc.qx.internal.cloudapp.net>
2021-09-21 02:39:02 +0000

    Update

---
+++ task1/cleanup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+filename=$1
+
+IFS=';'
+
+while read  email DOB groups shared 
+do
+
+		cd /
+
+                firstChar=${email:0:1}
+                stripEmail=${email%@*}
+                lastname=${stripEmail#*.}
+                username=$firstChar$lastname
+		
+		sudo userdel $username
+
+		sudo groupdel
+		
+		IFS=","
+        	for value in $groups
+        	do
+                        sudo groupdel $value
+       		done
+
+		IFS=";"
+
+		sudo rm -r $shared
+		sudo rm -r /home/$username
+ 	
+done < $filename
